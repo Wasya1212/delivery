8120da2614caba8a36b05b2c4dbd7b77fb23d447
UkrainetsVasyl <vukrainets@logitech.com>
2023-02-19 00:25:57 +0200

    added markers

--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,53 @@
-import React, { useEffect, useRef } from 'react';
-import mapboxgl from 'mapbox-gl';
+import React, { useState, useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoid2FzeWExMjEyIiwiYSI6ImNrNXRmdDJwYTB2ajQzZW11N21samJ2cnUifQ.7lV47ah1DXWnbVY7JhGg-g';
+mapboxgl.accessToken = "pk.eyJ1Ijoid2FzeWExMjEyIiwiYSI6ImNrNXRmdDJwYTB2ajQzZW11N21samJ2cnUifQ.7lV47ah1DXWnbVY7JhGg-g";
 
-const App = () => {
-  const mapContainer = useRef(null);
+const MapboxMap = () => {
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+  const [map, setMap] = useState<mapboxgl.Map>();
 
-  useEffect(() => {
+  const markerData = [
+    { label: "A", lng: -74.0059, lat: 40.7128 },
+    { label: "B", lng: -118.2437, lat: 34.0522 },
+    { label: "C", lng: -87.6298, lat: 41.8781 },
+  ];
+
+  const createMarkers = (map: mapboxgl.Map) => {
+    markerData.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(new mapboxgl.Popup().setHTML(`<h3>${marker.label}</h3>`))
+        .addTo(map);
+    });
+  };
+
+  const initializeMap = () => {
+    if (!mapContainerRef.current) return;
+
     const map = new mapboxgl.Map({
-      container: mapContainer.current || '',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-122.4194, 37.7749],
-      zoom: 12,
+      container: mapContainerRef.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-95.7129, 37.0902],
+      zoom: 3,
     });
 
-    return () => {
-      map.remove();
-    };
+    setMap(map);
+
+    map.on("load", () => {
+      createMarkers(map);
+    });
+  };
+
+  useEffect(() => {
+    initializeMap();
   }, []);
 
-  return <div ref={mapContainer} style={{ width: '100%', height: '100%' }} />;
+  return (
+    <div className="App">
+      <div ref={mapContainerRef} className="map-container" />
+    </div>
+  );
 };
 
-export default App;
+export default MapboxMap;
